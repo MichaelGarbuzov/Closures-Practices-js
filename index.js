7653c540179e32db59afd462abdfaf4a52711e2a
Michael Garbuzov <michaelgarbuzov1994@gmail.com>
2023-01-06 18:22:42 +0200

    update the js file

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
-(function formModal() {
+const popup = (function () {
   const form = document.querySelector("form");
   const nameInput = document.querySelector("#name");
   const emailInput = document.querySelector("#email");
   const ageInput = document.querySelector("#age");
+  const modal = document.querySelector(".modal");
+  const nameSpan = document.querySelector("#modal-name");
+  const emailSpan = document.querySelector("#modal-email");
+  const ageSpan = document.querySelector("#modal-age");
 
-  form.addEventListener("submit", function (event) {
-    // Prevent the form from being submitted
-    event.preventDefault();
+  function isEmail(email) {
+    return /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(email);
+  }
 
+  function validateForm() {
     const name = nameInput.value;
     const email = emailInput.value;
     const age = ageInput.value;
 
     if (name.length < 2) {
       alert("Name must be at least 2 characters long");
+      return false;
     } else if (isNaN(age)) {
       alert("Age must be a number");
+      return false;
     } else if (!isEmail(email)) {
       alert("Email must be a valid email address");
-    } else {
-      const modal = document.querySelector(".modal");
-      const nameSpan = document.querySelector("#modal-name");
-      const emailSpan = document.querySelector("#modal-email");
-      const ageSpan = document.querySelector("#modal-age");
-
-      nameSpan.textContent = name;
-      emailSpan.textContent = email;
-      ageSpan.textContent = age;
-
-      // Display the modal
-      modal.style.display = "block";
+      return false;
     }
-  });
-
-  // When the user clicks anywhere outside the modal, close it
-  window.addEventListener("click", function (event) {
-    const modal = document.querySelector(".modal");
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  });
-
-  function isEmail(email) {
-    return /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(email);
+    return true;
   }
+
+  function openModal() {
+    nameSpan.textContent = nameInput.value;
+    emailSpan.textContent = emailInput.value;
+    ageSpan.textContent = ageInput.value;
+    modal.style.display = "block";
+  }
+
+  function closeModal() {
+    modal.style.display = "none";
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (validateForm()) {
+      openModal();
+    }
+  }
+
+  form.addEventListener("submit", handleFormSubmit);
+  window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+      closeModal();
+    }
+  });
+
+  return {
+    openModal: openModal,
+    closeModal: closeModal,
+  };
 })();
